2231ad9becb79ea0b24816ceccf1d3b906bcfbd4
Tim Ings <tim@tim-ings.com>
2021-09-12 16:28:34 +0800

    remove old files

--- src/domain/channel/parse-stack.ts
+++
@@ -1,2 +0,0 @@
-import { ChannelResourceReference } from '../types';
-import { ChannelType, ChannelCategoryPermissions, TextChannelPermissions, VoiceChannelPermissions, StageChannelPermissions, AnnouncementChannelPermissions, PermissionOverrideStatus } from './types';
