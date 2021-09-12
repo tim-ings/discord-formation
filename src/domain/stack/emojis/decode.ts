58447aa6059b40ffae16b771811f4fdd792bb35c
Tim Ings <tim@tim-ings.com>
2021-09-12 16:28:24 +0800

    remove old structure

--- src/domain/stack/emojis/decode.ts
+++
@@ -1,8 +0,0 @@
-import * as t from 'io-ts';
-
-export type EmojisStack = t.TypeOf<typeof EmojisStackCodec>;
-
-export const EmojisStackCodec = t.array(t.type({
-  Name: t.string,
-  Image: t.string,
-}));
